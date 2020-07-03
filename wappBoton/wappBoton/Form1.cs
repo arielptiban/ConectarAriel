b3174be77d1b2e238e10c4c5ee8516889d92e56d
erickceva <61927150+erickceva@users.noreply.github.com>
2020-07-03 12:24:15 -0500

    nuevo cambio

--- wappBoton/wappBoton/Form1.cs
+++ wappBoton/wappBoton/Form1.cs
@@ -20,6 +20,7 @@ namespace wappBoton
         private void button1_Click(object sender, EventArgs e)
         {
             MessageBox.Show("hola");
+            MessageBox.Show("Si se puede");
         }
     }
 }
